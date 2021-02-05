6aa984bc44a1a000d7defd729bec7343d37559c0
dseum22 <dseum22@gmail.com>
2021-02-05 16:06:49 -0600

    Initial react version

---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <divç>
+            <h1>Contact me</h1>
+            <form className='mt-3'>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                    <label for="floatingInput">Email address</label>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Contact
