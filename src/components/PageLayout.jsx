8df577f8e7d937e5f5c27b4f1c90968737848fdd
dseum22 <dseum22@gmail.com>
2021-04-03 11:26:45 -0500

    Minor color improvements

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -4,7 +4,7 @@ const PageLayout = (props) => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-4 '>
             <div>
-            <span className='text-4xl sm:text-5xl font-bold border-b-4 border-gray-900'>{props.name}</span>
+            <span className='text-4xl sm:text-5xl font-bold border-b-4 border-gray-700 !leading-tight'>{props.name}</span>
             </div>
             <div className='mt-4 sm:mt-5 lg:mt-0 lg:col-span-3'>
                 {props.children}
