7e9d698eafdd1170f8d62b891f3904c86c22de76
dseum22 <dseum22@gmail.com>
2021-04-11 22:57:28 -0500

    Enabled Tailwind JIT compiler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
